f83f4d0ded7d14773fcfa2557a237bb648782537
huhaoo <qq826538400@gmail.com>
2019-05-06 09:27:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cd ~
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/huhaoo/configs.git
 cp configs/.zshrc .zshrc
 
