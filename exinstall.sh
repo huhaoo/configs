d6247828da88cf0076a33ccfb50b0040fe916d3a
huhaoo <qq826538400@gmail.com>
2019-05-08 09:11:49 +0800

    Create exinstall.sh

---
+++ exinstall.sh
@@ -0,0 +1,12 @@
+cd ~
+
+sudo apt install gnugo
+sudo apt install gnuchess
+sudo apt install greed
+sudo apt install yum
+sudo apt install gnome-tweak-tool
+sudo apt install gnome-shell-extensions
+sudo apt install curl
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+pip install opencv-python
