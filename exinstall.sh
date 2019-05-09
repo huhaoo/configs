f4f61db90302a34c97f670197c0444283330dd0c
huhaoo <qq826538400@gmail.com>
2019-05-09 14:28:01 +0800

    Update exinstall.sh

--- exinstall.sh
+++ exinstall.sh
@@ -11,3 +11,8 @@ sudo apt install python-pip
 sudo apt install python3-pip
 sudo pip install cyaron
 sudo pip install opencv-python
+sudo apt install gnuplot
+sudo apt install sl
+sudo apt install figlet
+sudo apt install npm
+sudo npm install hexo-cli -g
