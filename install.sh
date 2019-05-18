c0db9f6be8ede74ed8ea607437d40a5fedf4b2bd
huhaoo <qq826538400@gmail.com>
2019-05-18 14:07:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ sudo apt install python
 sudo apt install typora
 sudo apt install rar
 sudo apt install unrar
+sudo apt install apcalc
 
 cd ~
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
